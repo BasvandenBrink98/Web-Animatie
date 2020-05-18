f701c55582174ccd86f2bad4293af2e3358784a4
BasvandenBrink98 <43438614+BasvandenBrink98@users.noreply.github.com>
2020-05-18 18:58:10 +0200

    eerste aanpassing week3

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,13 @@ function groot(){
   document.getElementById('BG_circle').classList.toggle('groot');
   console.log("groot");
 }
+
+
+var redPoly = document.querySelector('.st3');
+
+redPoly.addEventListener('mouseover', kleur);
+
+function kleur() {
+  redPoly.classList.toggle('kleur');
+
+}
