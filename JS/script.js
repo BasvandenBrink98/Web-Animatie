b083f942bbd12c44d12727ad5401881c68f2c0e4
BasvandenBrink98 <43438614+BasvandenBrink98@users.noreply.github.com>
2020-05-23 18:57:49 +0200

    Animatie toevoeging

--- JS/script.js
+++ JS/script.js
@@ -2,15 +2,19 @@ document.getElementById('bttn').addEventListener("click", groot);
 
 function groot(){
   document.getElementById('BG_circle').classList.toggle('groot');
+  document.getElementById('Red_circle').classList.toggle('Red_circle_active');
   console.log("groot");
+  document.getElementById('Blue_Polygon').classList.toggle('Blue_Polygon_animation');
+  console.log("red circle Red_circle_active");
 }
 
 
-var redPoly = document.querySelector('.st3');
-
-redPoly.addEventListener('mouseover', kleur);
-
-function kleur() {
-  redPoly.classList.toggle('kleur');
-
-}
+// var redPoly = document.querySelector('.st3');
+//
+// redPoly.addEventListener('mouseover', kleur);
+//
+// function kleur() {
+//   redPoly.classList.toggle('kleur');
+//
+// }
+console.log("test");
