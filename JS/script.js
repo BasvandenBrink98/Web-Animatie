d50fec8857f5d80094d29efbe1586fdfe4b5674c
BasvandenBrink98 <43438614+BasvandenBrink98@users.noreply.github.com>
2020-06-24 14:59:32 +0200

    Animatie uitwerking

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,34 @@
-document.getElementById('bttn').addEventListener("click", groot);
+document.getElementById('bttn').addEventListener("click", start);
 
-function groot(){
-  document.getElementById('BG_circle').classList.toggle('groot');
+function start(){
   document.getElementById('Red_circle').classList.toggle('Red_circle_active');
-  console.log("groot");
-  document.getElementById('Blue_Polygon').classList.toggle('Blue_Polygon_animation');
-  document.getElementById('Red_Polygon').classList.toggle('Red_Polygon_animation');
-  document.getElementById('Green_Polygon').classList.toggle('Green_Polygon_animation');
-  console.log("red circle Red_circle_active");
+  document.getElementById('Blue_Polygon').classList.add('Blue_Polygon_animation');
+  document.getElementById('Red_Polygon').classList.add('Red_Polygon_animation');
+  document.getElementById('Green_Polygon').classList.add('Green_Polygon_animation');
+  document.getElementById('Yellow_Polygon').classList.add('Yellow_Polygon_animation');
+  document.getElementById('Blue_Polygon').classList.remove('outOfView');
+  document.getElementById('Red_Polygon').classList.remove('outOfView');
+  document.getElementById('Green_Polygon').classList.remove('outOfView');
+  document.getElementById('Yellow_Polygon').classList.remove('outOfView');
+  document.getElementById('instruction').classList.remove('hidden');
 }
 
+window.addEventListener("keydown", keyEvents);
 
-// var redPoly = document.querySelector('.st3');
-//
-// redPoly.addEventListener('mouseover', kleur);
-//
-// function kleur() {
-//   redPoly.classList.toggle('kleur');
-//
-// }
-console.log("test");
+function keyEvents(e){
+  let keyCode = e.keyCode;
+  let letters = document.querySelectorAll('.letter');
+  if (keyCode === 20){
+    console.log("keydown");
+    letters.forEach(element => element.classList.add('letterAnimatie'));
+    letters.forEach(element => element.removeAttribute('id'));
+
+    // document.querySelectorAll('.letter').forEach(element => element.classList.add('letterAnimatie'));
+    // document.querySelectorAll('.letter').forEach(element => element.removeAttribute('id'));
+  }
+
+  if(event.keyCode === 57){
+    console.log("spatie");
+  }
+
+}
